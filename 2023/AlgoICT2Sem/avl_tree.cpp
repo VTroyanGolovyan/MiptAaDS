63dda1d69435b8c70bd4c75d263d28a6f556fc18
vhdev <30373406+VladislavHacker@users.noreply.github.com>
2023-03-06 19:41:11 +0300

    Update avl_tree.cpp

--- 2023/AlgoICT2Sem/avl_tree.cpp
+++ 2023/AlgoICT2Sem/avl_tree.cpp
@@ -1,96 +1,204 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
 
-class DST {
-public:
-  DST(size_t n) : parent_(n), rank_(n, 0) {
-    for (size_t i = 0; i < n; ++i) {
-      parent_[i] = i;
+
+template <typename KeyT, typename ValueT> 
+class AVLTree {
+  struct Node {
+    int16_t height;
+    KeyT key;
+    ValueT value;
+    Node* left;
+    Node* right;
+    Node(KeyT key, ValueT value) 
+      : key(key), value(value), height(1), left(nullptr), right(nullptr) {
     }
-  }
+  };
 
-  bool IsSame(size_t u, size_t v) {
-    return Find(u) == Find(v);
-  }
+  public:
+    AVLTree() : root_(nullptr) {
+    }
 
-  size_t Find(size_t v) { // ищем корень
-    if (parent_[v] == v) return v;
-    parent_[v] = Find(parent_[v]); // эвристика сжатия пути
-    return parent_[v]; 
-  }
+    void Insert(KeyT key, ValueT value) {
+      root_ = Insert(root_, key, value);
+    }
 
-  void Union(size_t u, size_t v) { 
-    size_t root_u = Find(u);
-    size_t root_v = Find(v);
-    if (root_u != root_v) { // используем ранговую эвристику
-      if (rank_[root_u] < rank_[root_v]) {
-        std::swap(root_v, root_u);
+    void Erase(KeyT key) {
+      root_ = Erase(root_, key);
+    }
+
+    bool Contains(KeyT key) {
+      return Get(key) != nullptr;
+    }
+
+    ValueT* Get(KeyT key) {
+      Node* current = root_;
+      while (current != nullptr) {
+        if (current->key == key) {
+          return &current->value;
+        }
+        current = key < current->key ? current->left : current->right;
       }
-      parent_[root_v] = root_u; // подвесили
-      if (rank_[root_u] == rank_[root_v]) {
-        rank_[root_u] += 1; // старший брат имеет ранг на 1 больш!
+      return nullptr;
+    }
+
+    void Clear() {
+       Clear(root_);
+       size_ = 0; 
+       root_ = nullptr;
+    }
+
+    ~AVLTree() {
+      Clear(root_);
+    }
+
+    size_t Size() {
+      return size_;
+    }
+
+  private:
+
+    void Clear(Node* node) {
+      if (node == nullptr) {
+        return;
       }
+      Clear(node->left);
+      Clear(node->right);
+      delete node;
     }
-  }  
 
-private:
-  std::vector<size_t> parent_;
-  std::vector<size_t> rank_;
+    Node* Insert(Node* node, KeyT key, ValueT value) {
+      if (node == nullptr) {
+        ++size_;
+        return new Node(key, value);
+      }
+      if (key == node->key) {
+        node->value = value;
+        return node;
+      } 
+      if (key < node->key) {
+        node->left = Insert(node->left, key, value);
+      } else {
+        node->right = Insert(node->right, key, value);
+      }
+      return Balance(node);
+    }
+
+    Node* Erase(Node* node, KeyT key) {
+      if (node == nullptr) {
+        return node;
+      }
+
+      if (key < node->key) {
+        node->left = Erase(node->left, key);
+        return Balance(node);
+      }
+      if (key > node->key) {
+        node->right = Erase(node->right, key);
+        return Balance(node);
+      }
+
+      --size_;
+      if (node->right == nullptr) {
+        Node* left_child = node->left;
+        delete node;
+        return left_child;
+      }
+
+      Node* right_min = FindMin(node->right);
+      node->right = DetachMin(node->right);
+      std::swap(right_min->key, node->key);
+      std::swap(right_min->value, node->value);
+      delete right_min;
+      return Balance(node);
+    }
+
+    Node* Balance(Node* node) {
+      ActualizeHeight(node);// при insert/Detach всегда!
+      int16_t bf = BalanceFactor(node);
+      if (bf == -2) {
+        if (BalanceFactor(node->left) == 1) {
+          node->left = RotateLeft(node->left);
+        }
+        return RotateRight(node);
+      } else if (bf == 2) {
+        if (BalanceFactor(node->right) == -1) {
+          node->right = RotateRight(node->right);
+        }
+        return RotateLeft(node);
+      }
+      return node;
+    }
+
+    Node* RotateLeft(Node* node) {
+      Node* new_parent = node->right;
+      node->right = new_parent->left;
+      new_parent->left = node;
+      ActualizeHeight(node);
+      ActualizeHeight(new_parent);
+      return new_parent;
+    }
+
+    Node* RotateRight(Node* node) {
+      Node* new_parent = node->left;
+      node->left = new_parent->right;
+      new_parent->right = node;
+      ActualizeHeight(node);
+      ActualizeHeight(new_parent);
+      return new_parent;
+    }
+
+    Node* FindMin(Node* node) {
+      Node* curr = node;
+      while (curr != nullptr && curr->left != nullptr) {
+        curr = curr->left;
+      }
+      return curr;
+    }
+
+    Node* DetachMin(Node* node) {
+      if (node == nullptr) {
+        return nullptr;
+      }
+      if (node->left == nullptr) {
+        return node->right;
+      }
+      node->left = DetachMin(node->left);
+      return Balance(node);
+    }
+
+    int16_t BalanceFactor(Node* node) {
+      if (node == nullptr) {
+        return 0;
+      }
+      return (int16_t)Height(node->right) - (int16_t)Height(node->left);
+    }
+
+    void ActualizeHeight(Node* node) {
+      if (node == nullptr) {
+        return;
+      }
+      node->height = 1 + std::max(Height(node->left), Height(node->right));
+    }
+
+    int16_t Height(Node* node) {
+      if (node == nullptr) {
+        return 0;
+      }
+      return node->height;
+    }
+
+    Node* root_;
+    size_t size_{0};
 };
-
-
-struct Edge {
-  size_t from;
-  size_t to;
-  size_t weight;
-};
-
-std::vector<Edge> MSTKruscal(size_t n, std::vector<Edge>& edges) {
-
-  std::sort(
-    edges.begin(), 
-    edges.end(),
-    [](const Edge& e1, const Edge& e2) {
-      return e1.weight < e2.weight;
-    }
-  );
-
-  DST dst_vertexes(n);
-  std::vector<Edge> answer;
-  answer.reserve(n - 1);
-  size_t founded = 0;
-  for (auto& edge : edges) {
-    if (!dst_vertexes.IsSame(edge.from, edge.to)) {
-      ++founded;
-      dst_vertexes.Union(edge.from, edge.to);
-      answer.push_back(edge);
-    }
-    if (founded == n - 1) {
-      break;
-    }
-  }
-  return answer;
-}
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  std::vector<Edge> edges;
-  for (size_t i = 0; i < m; ++i) {
-    size_t from, to, weight;
-    std::cin >> from >> to >> weight;
-    edges.push_back({from, to, weight});
+  AVLTree<int, int> tree;
+  for (int i = 0; i < 1000000; ++i) {
+    tree.Insert(i, i);
   }
-
-  auto mst = MSTKruscal(n, edges);
-  std::cout << "MST:\n";
-  for (auto& edge : mst) { 
-    std::cout << edge.from << " " << edge.to << " " << edge.weight << std::endl;
+  for (int i = 200000; i < 600000; ++i) {
+     tree.Erase(i);
   }
-  std::cout << "MST weight:\n";
-  std::cout << std::accumulate(mst.begin(), mst.end(), 0ull, [](size_t acc, const Edge& edge) {
-    return acc + edge.weight;
-  });
+  std::cout << tree.Size();
+  return 0;
 }
